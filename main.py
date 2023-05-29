b9d292d1eed8d2f7f882e611e5dced117917300e
Raj <60320966+Raj-Offic@users.noreply.github.com>
2023-05-29 21:14:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import demomodule as mod
+
+print(mod.add(3, 6))
+print(mod.sub(3, 6))
---
+++ slice.py
@@ -0,0 +1,21 @@
+list1=[1,2,3,4,'apple', 'banana', 'mango']
+strings ='abcdefghijklmnopqrstuvwxyz1234567890'
+
+print(strings[0])
+
+
+print(strings[::4])  #slice operator prints the string every four letter
+
+
+print(strings[:4]) # prints till the (n-1) th term
+
+
+print(strings[4:])  # prints from the n th term
+
+
+print(strings[4::]) # same as the previous one : )
+
+print(strings[2:9]) # prints the text inbetween
+
+
+#   start:stop:step
